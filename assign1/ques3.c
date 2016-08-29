f2a68b792255c824f6ea82c8f85363a9828cc59a
Harsh Shah <harsh.shah.jayesh@gmail.com>
2016-08-29 17:51:00 +0530

    Question 3 with loop

---
+++ assign1/ques3.c
@@ -0,0 +1,94 @@
+#include <GL/glut.h>
+#include <stdio.h>
+#include <math.h>
+
+typedef struct Point {
+    float x;
+    float y;
+} Point;
+
+void fillBottom(float startx, float starty, float height, float width, int n)
+{
+    while (starty - height >= -0.9 + height * n) {
+        glBegin(GL_POLYGON);
+        glColor3f(0, 1, 0);
+
+        glVertex3f(startx, starty, 0);
+        glVertex3f(startx, starty - height, 0);
+        glVertex3f(startx - width, starty - height - width, 0);
+        glVertex3f(startx - width, starty - width, 0);
+
+        glEnd();
+        glFlush();
+        starty = starty - height;
+    }
+}
+
+Point drawStaircase(Point start, float height, float width)
+{
+    float startx = start.x;
+    float starty = start.y;
+
+    glBegin(GL_POLYGON);
+    glColor3f(1, 0, 0);
+
+    glVertex3f(startx, starty, 0);
+    glVertex3f(startx, starty + height, 0);
+    glVertex3f(startx - 2 * height, starty + 3 * height, 0);
+    glVertex3f(startx - 2 * height, starty + 2 * height, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0, 0, 1);
+
+    glVertex3f(startx, starty + height, 0);
+    glVertex3f(startx + width, starty + height + width, 0);
+    glVertex3f(startx - 2 * height + width, starty + 3 * height + width, 0);
+    glVertex3f(startx - 2 * height, starty + 3 * height, 0);
+    glEnd();
+
+    glFlush();
+    Point end;
+    end.x = startx + width;
+    end.y = starty + height + width;
+    return end;
+}
+
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    Point start;
+    start.x = -0.3;
+    start.y = -0.9;
+
+    float height = 0.22;
+    float width = 0.19;
+
+    int n = 4;
+
+    for (int i = 0; i < n; i++) {
+        start = drawStaircase(start, height, width);
+        fillBottom(start.x, start.y, height, width, i);
+    }
+}
+
+void init2D(float r, float g, float b)
+{
+    glClearColor(r, g, b, 0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Question 3");
+    init2D(0.75, 0.75, 0.75);
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
+
