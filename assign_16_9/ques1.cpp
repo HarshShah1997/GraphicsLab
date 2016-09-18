e978cead0144f8f8a36c20092b00d8eb55df70c1
Harsh Shah <harsh.shah.jayesh@gmail.com>
2016-09-18 19:27:05 +0530

    Rotation working properly

--- assign_16_9/ques1.cpp
+++ assign_16_9/ques1.cpp
@@ -1,6 +1,8 @@
 #include <GL/glut.h>
 #include <cmath>
 #include <unistd.h>
+#include <time.h>
+#include "../matrix.h"
 
 class Point {
     public:
@@ -30,11 +32,40 @@ class Circle {
                 glVertex2f(radius * cos(angle) + center -> x, radius * sin(angle) + center -> y);
             }
             glEnd();
+
             glBegin(GL_LINES);
-            glVertex2f(-0.7, 0.3);
-            glVertex2f(0.3, -0.7);
+            glVertex2f(-0.7 - 0.3 / sqrt(2), 0.7 - 0.3 / sqrt(2));
+            glVertex2f(0.3 - 0.3 / sqrt(2), -0.3 - 0.3 / sqrt(2));
             glEnd();
-            glutSwapBuffers();
+            glFlush();
+        }
+
+        void drawLine(float angle) {
+            Matrix *q = new Matrix(3, 1);
+            q -> elementAt[0][0] = center -> x + radius / sqrt(2);
+            q -> elementAt[1][0] = center -> y + radius / sqrt(2);
+            
+            Matrix* rotate = new Matrix(3, 3);
+            rotate -> elementAt[0][0] = cos(angle);
+            rotate -> elementAt[0][1] = -sin(angle);
+            rotate -> elementAt[1][0] = cos(angle);
+            rotate -> elementAt[1][1] = sin(angle);
+
+            Matrix* translate = new Matrix(3, 1);
+            translate -> elementAt[0][0] = - (center -> x);
+            translate -> elementAt[1][0] = - (center -> y);
+
+            Matrix *org = new Matrix(3, 1);
+            org -> elementAt[0][0] = center -> x;
+            org -> elementAt[1][0] = center -> y;
+
+            Matrix *p = add(org, multiply(rotate, add(translate, q)));
+
+            glBegin(GL_LINES);
+            glVertex2f(p -> elementAt[0][0], p -> elementAt[1][0]);
+            glVertex2f(center -> x, center -> y);
+            glEnd();
+            glFlush();
         }
 };
 
@@ -44,6 +75,11 @@ void display(void)
 
     float x = -0.7;
     float y = 0.7;
+    float angle = 0;
+
+    struct timespec tim1, tim2;
+    tim1.tv_sec = 0;
+    tim1.tv_nsec = 250000000L; //Half second
 
     for (int i = 0; i < 10; i++) {
         float rand1 = 65.0 / 255;
@@ -53,9 +89,12 @@ void display(void)
         Point *center = new Point(x, y, 0);
         Circle *circle = new Circle(center, 0.3);
         circle -> drawCircle();
+        circle -> drawLine(angle);
         x += 0.1;
         y -= 0.1;
-        sleep(1);
+        angle -= 0.4;
+        
+        nanosleep(&tim1, &tim2);
     }
 
 }
@@ -63,7 +102,7 @@ void display(void)
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
     glutCreateWindow("Question 1");
     glClearColor(0.75, 0.75, 0.75, 1);
